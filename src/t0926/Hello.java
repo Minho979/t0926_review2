40127878453852f08499b99a2c5f66d0aa7472da
Kim, Minho <minho9779@gmail.com>
2022-09-27 13:23:37 +0900

    first

---
+++ src/t0926/Hello.java
@@ -0,0 +1,8 @@
+package t0926;
+
+public class Hello {
+
+    public void say() {
+        System.out.println("hello");
+    }
+}
---
+++ src/t0926/Main.java
@@ -0,0 +1,8 @@
+package t0926;
+
+public class Main {
+    
+    public static void main(String[] args) {
+        System.out.println("hello world");
+    }
+}
